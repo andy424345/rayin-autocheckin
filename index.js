aa0922dafe50bbccdff7c2309ac15ee4d7ca4267
Andy Cheng <andy424345@gmail.com>
2023-06-01 16:56:39 +0800

    updated

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 require('dotenv').config()
 const express = require('express')
-const cors = require('cors')
 const server = express()
-const { LINE_NOTIFY_TOKEN, CUSTNAME, CORS_AUTH } = process.env
-const schedule = require('node-schedule')
+const { LINE_NOTIFY_TOKEN } = process.env
 const axios = require('axios')
 const FormData = require('form-data')
 
  /**
-  * 推播LINE Notify
-  * @param {String} message   
+  * 推播LINE Notify  
+  * @param {String} token  
+  * @param {String} message      
   */
-var notify = async (message) => {    
-    if (!LINE_NOTIFY_TOKEN) return
+var notify = async (token, message) => {    
+    if (!token) {
+        token = LINE_NOTIFY_TOKEN
+    }
     const data = new FormData()
     data.append('message', message)
     await axios({
@@ -20,19 +21,21 @@ var notify = async (message) => {
         url: 'https://notify-api.line.me/api/notify',
         headers: { 
             'Content-Type': 'application/x-www-form-urlencoded',
-            'Authorization': `Bearer ${LINE_NOTIFY_TOKEN}`
+            'Authorization': `Bearer ${token}`
         },
         data
     })
 }
 
 /**
- * 
- * @param {String} chkType 
+ * 執行簽到
+ * @param {String} token LINE推播權杖
+ * @param {Stirng} custname 簽到人員
+ * @param {String} chkType 0簽到 1簽退
  */
-var signin = async (chkType) => {
+var signin = async (token, custname, chkType) => {
     const data = new FormData()    
-    data.append('custname', CUSTNAME)
+    data.append('custname', custname)
     data.append('chkType', chkType)
     let res = await axios({
         method: 'POST',
@@ -40,37 +43,31 @@ var signin = async (chkType) => {
         headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' },
         data
     })
-    await notify(res.data)
+    await notify(token, `\n簽到人員:${custname}\n結果:${res.data}`)
+    return res.data
 }
 
-let rule = new schedule.RecurrenceRule()    
-rule.hour = 8
-rule.minute = Math.floor(Math.random() * 10)
-rule.second = Math.floor(Math.random() * 60) 
-
-const work = schedule.scheduleJob(rule, async () => {
-    await signin('0')
-})
-
-rule = new schedule.RecurrenceRule()    
-rule.hour = 17
-rule.minute = Math.floor(Math.random() * 10)
-rule.second = Math.floor(Math.random() * 60) 
-
-const offwork = schedule.scheduleJob(rule, async () => {
-    await signin('1')
-})
-
-setTimeout(async () => {
-    await notify(`\n自動簽到已啟動\nCUSTNAME: ${CUSTNAME}`)
-}, 10)
-
 server.use(express.json())
-server.use(cors((req, callback) => {
-    callback(null, true)
-}))
+
 server.all('/', (req, res) => {
     res.send('Your bot is alive!')
 })
+
+server.get('/checkin', async (req, res) => {
+    const { name, type, token } = req.query        
+    if (!name || !type) {
+        console.log('err')
+        res.statusCode = 404
+        res.send('404 NOT FOUND')
+        return
+    }
+    const data = await signin(token, name, type)
+    res.send(data)
+})
+
 const port = process.env.PORT || 3001
 server.listen(port, () => console.log(`[已準備就緒] port: ${port}`))
+
+setTimeout(async () => {
+    await notify(LINE_NOTIFY_TOKEN, `\n自動簽到已啟動`)
+}, 10)
